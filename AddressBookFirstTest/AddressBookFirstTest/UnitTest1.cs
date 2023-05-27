b7f75bb733ea619608d2ee8867eeecce5b3ba1e1
SergeyArakelov <innine@mail.ru>
2023-05-27 22:34:58 +0300

    Clean up

--- AddressBookFirstTest/AddressBookFirstTest/UnitTest1.cs
+++ AddressBookFirstTest/AddressBookFirstTest/UnitTest1.cs
@@ -1,14 +1,10 @@
-using System;
 using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Firefox;
-using OpenQA.Selenium.Support.UI;
 
-namespace SeleniumTests
+
+namespace AddressBookFirstTest
 {
     [TestFixture]
     public class UntitledTestCase
