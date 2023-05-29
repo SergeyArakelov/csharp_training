cbf012f8f36427e29684ec681e2b6e15dae91f0c
SergeyArakelov <innine@mail.ru>
2023-05-29 21:54:45 +0300

    Задание №4: Сделать всё то же самое, но для контактов

---
+++ AddressBookCreationUser/AddressBookCreationUser/AccountData.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookCreationUser
+{
+    internal class AccountData
+    {
+        private string login;
+        private string password;
+
+        public AccountData(string login, string password)
+        {
+            this.login = login;
+            this.password = password;
+
+        }
+
+        public string Login
+        {
+            get
+            {
+                return login;
+            }
+
+            set
+            {
+                login = value;
+            }
+
+        }
+
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+
+            set
+            {
+                password = value;
+            }
+
+        }
+
+
+    }
+}
---
+++ AddressBookCreationUser/AddressBookCreationUser/UserData.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookCreationUser
+{
+    class UserData
+    {
+        private string firstname = "";
+        private string secondname = "";
+
+        public UserData(string username)
+        {
+            firstname = firstname;
+            secondname = secondname;
+        }
+
+
+        
+        
+        public string FirstName
+        {
+            get
+            {
+                return firstname;
+            }
+            set
+            {
+                firstname = value;
+            }
+        }
+        public string SecondName
+        {
+            get
+            {
+                return secondname;
+            }
+            set
+            {
+                secondname = value;
+            }
+        }
+    }
+}
